66a5ea72355e9cc7f1f6aeeaf38b3a8293dffb16
Marcelo Pinto <marcelos.mail@gmail.com>
2017-05-19 23:39:17 -0300

    Removing image overlays on services list items

--- index.php
+++ index.php
@@ -23,52 +23,19 @@
 	<ul class="services-list">
 		<li>
 			<h2>Criação de sites</h2>
-			<!--a class="anchor-block"-->
-				<img src="img/responsive-web-design.svg" alt="Ilustração de um desktop, um tablet e um telefone celular" />
-				<!--div class="overlay">
-					<div class="overlay-caption">
-            <span>Criação</span>
- 					  <span class="shade-right"></span>
-					  <span class="underline-right"></span>
- 					  <span class="underline-left"></span>
- 					  <span class="shade-left"></span>
-  				</div>
-				</div>
-			</a-->
+			<img src="img/responsive-web-design.svg" alt="Ilustração de um desktop, um tablet e um telefone celular" />
 			<p>Construção de websites a partir de uma ideia ou tema de sua escolha. Este é ponto de partida para que seu projeto pessoal, portfólio profissional ou empresa tenha uma presença online.</p>
 			<p>Saiba mais sobre o serviço de <a class="text-link">Criação de sites</a>.</p>
 		</li>
 		<li>
 			<h2>Otimização de performance</h2>
-			<!--a class="anchor-block"-->
-				<img src="img/website-optimization.svg" alt="Ilustração de uma página web, um globo e uma engrenagem" />
-				<!-- div class="overlay">
-					<div class="overlay-caption">
-            <span>Performance</span>
- 					  <span class="shade-right"></span>
-					  <span class="underline-right"></span>
- 					  <span class="underline-left"></span>
- 					  <span class="shade-left"></span>
-  				</div>
-				</div>
-			</a-->
+			<img src="img/website-optimization.svg" alt="Ilustração de uma página web, um globo e uma engrenagem" />
 			<p>Alteração do código do site com o objetivo de reduzir o tempo de carregamento das páginas. Quanto mais rápida a navegação do usuário, maior a probabilidade de engajamento e conversão.</p>
 			<p>Conheça melhor o trabalho de <a class="text-link">Otimização de performance</a> das páginas de um site.</p>
 		</li>
 		<li>
 			<h2>Gestão de conteúdo</h2>
-			<!--a class="anchor-block"-->
-				<img src="img/content-management.svg" alt="Ilustração de uma página web, com seu conteúdo pintado em diferentes cores" />
-				<!-- div class="overlay">
-					<div class="overlay-caption">
-            <span>Gestão</span>
- 					  <span class="shade-right"></span>
-					  <span class="underline-right"></span>
- 					  <span class="underline-left"></span>
- 					  <span class="shade-left"></span>
-  				</div>
-				</div>
-			</a-->
+			<img src="img/content-management.svg" alt="Ilustração de uma página web, com seu conteúdo pintado em diferentes cores" />
 			<p>Modificações no conteúdo e layout do site, para que esteja sempre atualizado com a realidade de seu negócio. Atualmente o website é a interface de relacionamento mais utilizada pelo consumidor.</p>
 			<p>Veja quais são os serviços de <a class="text-link">Gestão de conteúdo</a> que podemos te oferecer.</p>
 		</li>
