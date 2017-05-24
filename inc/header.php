fdeda9f57bbd4a31e52f23438001cc44b22e43bb
Marcelo Pinto <marcelos.mail@gmail.com>
2017-05-24 20:18:16 -0300

    Added Google Analytics follow-up tag

--- inc/header.php
+++ inc/header.php
@@ -25,6 +25,18 @@
 
 	<body>
 
+		<!-- Google Analytics tag -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-50034609-2', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+
 		<!-- Header SVGs -->
 		<svg xmlns="http://www.w3.org/2000/svg" class="svg-defs" title="header svgs">
 			<symbol id="at-sign-grey" viewbox="0 0 24 23.99">
