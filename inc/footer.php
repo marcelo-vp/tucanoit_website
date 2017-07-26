2143b4ca02a199c520f0e62bd636eab0de95aa8d
Marcelo Pinto <marcelos.mail@gmail.com>
2017-07-26 17:32:37 -0300

    Renaming files and folders

--- inc/footer.php
+++ inc/footer.php
@@ -135,7 +135,8 @@
 		<!-- JS CALLS -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="js/slick.min.js"></script>
-		<script src="js/scripts.min.js"></script>
+		<script src="js/scripts.js"></script>
+		<!--script src="js/scripts.min.js"></script-->
 
 	</body>
 	<!-- BODY : END -->
